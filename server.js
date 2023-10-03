235d346a58512dd387dab2e23a8af54fed9fa62c
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2023-10-03 21:02:26 +0800

    new commit

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
+app.options('*', cors())
 
 app
   .use(bodyParser.json())
+  .use(cors())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-AllowHeaders", "Origin, X-Requested-With, Content-type, Accept")
     next();
   })
   .use('/', require('./routes'));
+  
 
 mongodb.initDb((err) => {
   if (err) {
